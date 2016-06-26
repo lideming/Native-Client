aad091cf9584cbdf9eb0df5d8ef36f81b56a864b
JasonBian <imaginezz@outlook.com>
2016-06-26 18:07:36 +0800

    add umeng analytics

--- DanMu/App.xaml.cs
+++ DanMu/App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using UmengAnalyticsNet;
+using UmengSDK;
+using System.Diagnostics;
 
 namespace DanmakuPie
 {
@@ -13,5 +16,18 @@ namespace DanmakuPie
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            UmengAnalyticsNet.Wpf.UmengAnalyticsApp.Initialize("576f96ce67e58eed3d002295", "website");
+            //Debug.WriteLine(UmengAnalyticsNet.OS.GetOSVersion().ToString());
+            //Debug.WriteLine(UmengAnalyticsNet.OS.GetOSString().ToString());
+            //Debug.WriteLine(UmengAnalyticsNet.Token.GetToken().ToString());
+            UmengAnalyticsNet.Wpf.AppInfo info = new UmengAnalyticsNet.Wpf.AppInfo();
+            UmengAnalyticsNet.Wpf.
+            Debug.WriteLine(info.GetAppVersion().ToString());
+            Debug.WriteLine(info.GetResolution().ToString());
+            Debug.WriteLine(info.GetUserId().ToString());
+        }
     }
 }
